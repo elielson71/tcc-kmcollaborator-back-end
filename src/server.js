b952fbcd8e3e1652fc920f8d8272fed1608846db
Eleilson Santos <elielsonboavista@gmail.com>
2021-11-04 13:04:15 -0300

    alteração na conexao com api

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ const app = express();
 
 //app.use(cors());
 app.use((req, res, next) => {
+
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
     next();
