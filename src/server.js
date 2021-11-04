f1b392e0a843ec59c275452315249d40735a47bb
Eleilson Santos <elielsonboavista@gmail.com>
2021-11-04 08:51:39 -0300

    cors

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const app = express();
 //app.use(cors());
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "127.0.0.1");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
     app.use(cors());
     next();
