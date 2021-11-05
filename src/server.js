f73e4a7156c4a57aaac6b5acd9f9133897c9bc77
Eleilson Santos <elielsonboavista@gmail.com>
2021-11-05 13:12:30 -0300

    api

--- src/server.js
+++ src/server.js
@@ -7,8 +7,8 @@ const app = express();
 app.use(cors());
 app.use((req, res, next) => {
     console.log(req.headers)
-    //res.header("Access-Control-Allow-Origin",'http://localhost:3000');
-    res.header("Access-Control-Allow-Origin",'*');
+    res.header("Access-Control-Allow-Origin",'http://localhost:3000'||'https://kmcollaborator.herokuapp.com');
+    //res.header("Access-Control-Allow-Origin",'*,');
     res.header("Access-Control-Allow-Credentials",'true');
     res.header("Access-Control-Allow-Headers", "Content-Type,authorization, X-Requested-With");
     //res.header('Content-Type', 'application/json');
