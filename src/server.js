bfc4d111ca4e6784118c2880d21c143743955fdf
Elielson <elielsonboavista@gmail.com>
2021-11-06 05:55:40 -0300

    OPTIONS

--- src/server.js
+++ src/server.js
@@ -9,31 +9,34 @@ const app = express();
 app.use((req, res, next) => {
     console.log(req.headers)
     //res.header("Access-Control-Allow-Origin","http://kmcollaborator.herokuapp.com");
-    res.header("Access-Control-Allow-Origin","http://kmcollaborator.herokuapp.com");
+    res.header("Access-Control-Allow-Origin", "http://kmcollaborator.herokuapp.com");
     //res.header("Access-Control-Allow-Origin",'*');
-    res.header("Access-Control-Allow-Credentials",'true');
-    res.header("Access-Control-Allow-Headers", "Content-Type,authorization, X-Requested-With")
-
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Credentials", 'true');
+    res.header("Access-Control-Allow-Headers", 
+    "Origin,Accept,Content-Type,authorization, X-Requested-With")
+    if (req.method === 'OPTIONS') {
+        res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+        return res.status(200).send({})
+    }
     app.use(cors());
     next();
 });
 
 app.use(express.json());
-async function isu(){
-    const user =await getUsuario()
+async function isu() {
+    const user = await getUsuario()
     user.lea
 
 }
-app.use('/v1/midias',express.static(path.resolve(__dirname,"upload")));
+app.use('/v1/midias', express.static(path.resolve(__dirname, "upload")));
 
-app.use('/v1/',require('./route/auth_route'))
-app.use('/v1/',require('./route/baseconhecimento_route'))
-app.use('/v1/',require('./route/usuarios_route'))
-app.use('/v1/',require('./route/grupo_route'))
-app.use('/v1/',require('./route/correcao_route'))
-app.use('/v1/',require('./route/profissional_route'))
-app.use('/v1/',require('./route/questions_route'))
-app.use('/v1/',require('./route/avaliacao_route'))
-app.use('/v1/',require('./route/departamento_route'))
+app.use('/v1/', require('./route/auth_route'))
+app.use('/v1/', require('./route/baseconhecimento_route'))
+app.use('/v1/', require('./route/usuarios_route'))
+app.use('/v1/', require('./route/grupo_route'))
+app.use('/v1/', require('./route/correcao_route'))
+app.use('/v1/', require('./route/profissional_route'))
+app.use('/v1/', require('./route/questions_route'))
+app.use('/v1/', require('./route/avaliacao_route'))
+app.use('/v1/', require('./route/departamento_route'))
 app.listen(process.env.PORT || 3001)
