aa88d09876ee5a5880b55d4d585b046ad3176a9d
Eleilson Santos <elielsonboavista@gmail.com>
2021-11-06 09:20:53 -0300

    lá vamos nós

--- src/infra/database.js
+++ src/infra/database.js
@@ -1,6 +1,6 @@
 // conexao banco de dados
 const pgp = require('pg-promise')();
-
+console.log(`${process.env.DATABASE_URL}`)
 const db = pgp(`${process.env.DATABASE_URL}`)
 /*const db = pgp({
     user:'postgres',
