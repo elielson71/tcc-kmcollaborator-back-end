4ddfd9a633b1d69d468319f0c9d4ea8f6f8a2d93
Elielson <elielsonboavista@gmail.com>
2021-11-06 06:15:08 -0300

    teste

--- src/server.js
+++ src/server.js
@@ -5,12 +5,12 @@ const { getUsuario } = require('./data/usuario_data');
 require('dotenv').config()
 const app = express();
 
-//app.use(cors());
+app.use(cors());
 app.use((req, res, next) => {
-    console.log(req.headers)
+    //console.log(req.headers)
     //res.header("Access-Control-Allow-Origin","http://kmcollaborator.herokuapp.com");
     res.header("Access-Control-Allow-Origin", "http://kmcollaborator.herokuapp.com");
-    //res.header("Access-Control-Allow-Origin",'*');
+    res.header("Access-Control-Allow-Origin",'*');
     res.header("Access-Control-Allow-Credentials", 'true');
     res.header("Access-Control-Allow-Headers", 
     "Origin,Accept,Content-Type,authorization, X-Requested-With")
@@ -23,11 +23,6 @@ app.use((req, res, next) => {
 });
 
 app.use(express.json());
-async function isu() {
-    const user = await getUsuario()
-    user.lea
-
-}
 app.use('/v1/midias', express.static(path.resolve(__dirname, "upload")));
 
 app.use('/v1/', require('./route/auth_route'))
