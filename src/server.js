22cb572c2d641ceb87f8801e2779012921db59ae
Eleilson Santos <elielsonboavista@gmail.com>
2021-11-05 23:00:57 -0300

    3000

--- src/server.js
+++ src/server.js
@@ -8,8 +8,8 @@ const app = express();
 //app.use(cors());
 app.use((req, res, next) => {
     console.log(req.headers)
-    res.header("Access-Control-Allow-Origin","http://kmcollaborator.herokuapp.com");
-    //res.header("Access-Control-Allow-Origin","http://localhost:3000");
+    //res.header("Access-Control-Allow-Origin","http://kmcollaborator.herokuapp.com");
+    res.header("Access-Control-Allow-Origin","http://localhost:3000");
     //res.header("Access-Control-Allow-Origin",'*,');
     res.header("Access-Control-Allow-Credentials",'true');
     res.header("Access-Control-Allow-Headers", "Content-Type,authorization, X-Requested-With")
